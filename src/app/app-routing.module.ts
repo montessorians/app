e3d402bf47eafaae8aa7f022fd36237dace3276f
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-05-17 02:29:17 +0800

    feat: update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './page/home/home.component';
+import { AssessmentComponent } from './page/assessment/assessment.component';
+import { EcashComponent } from './page/ecash/ecash.component';
+import { PeopleComponent } from './page/people/people.component';
+import { MeComponent } from './page/me/me.component';
+import { LoginComponent } from './page/login/login.component';
+import { LogoutComponent } from './page/logout/logout.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'assessment', component: AssessmentComponent },
+  { path: 'ecash', component: EcashComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'me', component: MeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
