7751628fbc32ca77208ae633344c4c30673ae957
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-05-17 02:25:49 +0800

    feat(src): add firebase keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCuNfQSkwl85bk38k4de_QR-DwBGL-069o',
+    authDomain: 'holy-child-montessori.firebaseapp.com',
+    databaseURL: 'https://holy-child-montessori.firebaseio.com',
+    projectId: 'holy-child-montessori',
+    storageBucket: 'holy-child-montessori.appspot.com',
+    messagingSenderId: '144464720189',
+    appId: '1:144464720189:web:4409bc5f8c231e1a6cd554',
+    measurementId: 'G-SZFPXXKY4J'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCuNfQSkwl85bk38k4de_QR-DwBGL-069o',
+    authDomain: 'holy-child-montessori.firebaseapp.com',
+    databaseURL: 'https://holy-child-montessori.firebaseio.com',
+    projectId: 'holy-child-montessori',
+    storageBucket: 'holy-child-montessori.appspot.com',
+    messagingSenderId: '144464720189',
+    appId: '1:144464720189:web:4409bc5f8c231e1a6cd554',
+    measurementId: 'G-SZFPXXKY4J'
+  }
 };
 
 /*
