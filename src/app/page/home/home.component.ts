1fba1784bdff8e03e16d84c7d26f2ab540ff5af3
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-05-18 03:23:16 +0800

    feat: update home

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -8,24 +8,68 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class HomeComponent implements OnInit {
 
-  user: { first_name: null, middle_name: null, last_name: null };
+  user: { first_name: null, middle_name: null, last_name: null, userId: null };
 
-  feeds = [
-    {
-      title: 'Sample Title',
-      content: 'Sample Content',
-      preview_image: 'https://data.whicdn.com/images/272258360/original.gif',
-      author: [
-        { first_name: 'Andresito', last_name: 'de Guzman' }
-      ],
-      timestamp: '2020-05-16T16:46:29.476Z'
-    }
-  ];
+  feeds = [];
+  feedLoading = true;
+  feedError = false;
 
   constructor(private db: AngularFirestore) { }
 
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('user'));
+    this.getFeed();
+  }
+
+  getFeed() {
+    this.feedLoading = true;
+    this.db.collection('feeds').get().subscribe(res => {
+      if (!res.empty) {
+        res.forEach(obj => {
+          const d = { id: null };
+          const data = obj.data();
+          Object.keys(data).forEach(k => { d[k] = data[k]; });
+          d.id = obj.id;
+          console.log(d);
+          this.feeds.push(d);
+        });
+        this.checkLikes();
+        localStorage.setItem('feeds', JSON.stringify(this.feeds));
+      }
+      this.feedLoading = false;
+    }, e => {
+      if (localStorage.getItem('feeds')) {
+        this.feeds = JSON.parse(localStorage.getItem('feeds'));
+      } else {
+        this.feedError = true;
+      }
+    });
+  }
+
+  checkLikes() {
+    this.feeds.forEach(obj => {
+      this.db.collection('feeds').doc(obj.id).collection('likes').doc(this.user.userId).get().subscribe(res => {
+        if (res.exists) {
+          obj.liked = true;
+        } else {
+          obj.liked = false;
+        }
+      });
+      return obj;
+    });
+    localStorage.setItem('feeds', JSON.stringify(this.feeds));
+  }
+
+  likeStatus(feed) {
+    this.db.collection('feeds').doc(feed.id).collection('likes').doc(this.user.userId).set({ timestamp: new Date() }).then(() => {
+      feed.liked = true;
+    });
+  }
+
+  unlikeStatus(feed) {
+    this.db.collection('feeds').doc(feed.id).collection('likes').doc(this.user.userId).delete().then(()=>{
+      feed.liked = false;
+    });
   }
 
 }
