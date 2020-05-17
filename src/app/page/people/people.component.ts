58b56e7f3f38cfaee98550b96e0855c222b6d36c
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-05-18 03:24:11 +0800

    feat: add admin list

--- src/app/page/people/people.component.ts
+++ src/app/page/people/people.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  loading = true;
+  adminList = [];
 
   ngOnInit(): void {
+    this.getAdmins();
+  }
+
+  getAdmins() {
+    this.loading = true;
+    this.db.collection('users', ref => ref.where('account_type', '==', 'admin')).get().subscribe(res => {
+      this.loading = false;
+      if (!res.empty) {
+        this.adminList = [];
+        res.forEach(obj => {
+          const d = { id: obj.id };
+          Object.keys(obj.data()).forEach(k => { d[k] = obj.data()[k]; });
+          this.adminList.push(d);
+        });
+      }
+    });
   }
 
 }
