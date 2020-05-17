fec5cf1a714111c0ada3c97f6303fb04b2c4e329
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-05-18 03:21:25 +0800

    feat: create navbar

---
+++ src/app/component/global/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.styl']
+})
+export class NavbarComponent implements OnInit {
+
+  @Input() title = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
