c2e5b6e0ae3f5895c114a586511635fa5eb3db98
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-05-18 03:24:40 +0800

    feat(app): update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
 import { HomeComponent } from './page/home/home.component';
 import { AssessmentComponent } from './page/assessment/assessment.component';
 import { EcashComponent } from './page/ecash/ecash.component';
 import { PeopleComponent } from './page/people/people.component';
 import { MeComponent } from './page/me/me.component';
+import { NotificationsComponent } from './page/notifications/notifications.component';
+import { ProfileComponent } from './page/profile/profile.component';
+import { AccountSettingsComponent } from './page/account-settings/account-settings.component';
+import { PostComponent } from './page/post/post.component';
 import { LoginComponent } from './page/login/login.component';
 import { LogoutComponent } from './page/logout/logout.component';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'assessment', component: AssessmentComponent },
-  { path: 'ecash', component: EcashComponent },
-  { path: 'people', component: PeopleComponent },
-  { path: 'me', component: MeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'logout', component: LogoutComponent }
+  { path: '', component: HomeComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'assessment', component: AssessmentComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'ecash', component: EcashComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'people', component: PeopleComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'me', component: MeComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'notifications', component: NotificationsComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'profile', component: ProfileComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'post/:id', component: PostComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'account/settings', component: AccountSettingsComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: 'login', component: LoginComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome }},
+  { path: 'logout', component: LogoutComponent, canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
