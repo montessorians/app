e9d528a33c5b86dda72ca8b12d7072a653672a76
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-05-18 03:23:27 +0800

    feat: update me

--- src/app/page/me/me.component.ts
+++ src/app/page/me/me.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-me',
   templateUrl: './me.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeComponent implements OnInit {
 
-  constructor() { }
+  firebaseUser = { photoURL: null, displayName: null, email: null };
+  user = { userId: null, photoURL: null, email: null,
+  first_name: null, last_name: null, account_type: null, student_no: null };
+
+  constructor(private afAuth: AngularFireAuth) {
+  }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user'));
+    this.afAuth.user.subscribe(res => {
+      this.firebaseUser = res;
+    });
   }
 
 }
