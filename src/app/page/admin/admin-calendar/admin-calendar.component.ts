35c6ced8f2eb7af9ab1498f93208fe47b523d146
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-06-11 22:53:10 +0800

    feat(page): create admin-calendar

---
+++ src/app/page/admin/admin-calendar/admin-calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-calendar',
+  templateUrl: './admin-calendar.component.html',
+  styleUrls: ['./admin-calendar.component.styl']
+})
+export class AdminCalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
