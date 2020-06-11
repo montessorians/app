64d676dd984eff46751b0ff07cda5e1309bb9770
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-06-11 22:52:41 +0800

    feat(page): create admin-registrar

---
+++ src/app/page/admin/admin-registrar/admin-registrar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-registrar',
+  templateUrl: './admin-registrar.component.html',
+  styleUrls: ['./admin-registrar.component.styl']
+})
+export class AdminRegistrarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
